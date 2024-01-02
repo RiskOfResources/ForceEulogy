5fa341dec218a2bd132af612c889e3db4ab58982
MarkTullius <mark_standerwick@live.com>
2024-01-02 21:47:33 +0000

    Publish all the stuff cos I forgot to ever do so

---
+++ ForceEulogy/src/Main.cs
@@ -0,0 +1,123 @@
+using RoR2;
+using R2API;
+using BepInEx;
+using BepInEx.Bootstrap;
+using BepInEx.Configuration;
+using UnityEngine.AddressableAssets;
+using RiskOfOptions;
+using RiskOfOptions.Options;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace PodRacing;
+
+[BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+
+public class ForceEulogy : BaseUnityPlugin
+{
+  public const string PluginGUID = PluginAuthor + "." + PluginName;
+  public const string PluginAuthor = "RiskOfResources";
+  public const string PluginName = "ForceEulogy";
+  public const string PluginVersion = "1.1.0";
+
+  public static ConfigEntry<int> amount { get; set; }
+  public static ConfigEntry<BooleanChoice> removeFromPool { get; set; }
+
+  public static ArtifactDef podRacing = ScriptableObject.CreateInstance<ArtifactDef>();
+  public static bool podRacingEnabled = false;
+  public static AssetBundle assets;
+  public static Sprite artifactOnIcon;
+  public static Sprite artifactOffIcon;
+  public ItemDef beads = Addressables.LoadAssetAsync<ItemDef>("RoR2/Base/LunarTrinket/LunarTrinket.asset").WaitForCompletion();
+  public ItemDef eulogy = Addressables.LoadAssetAsync<ItemDef>("RoR2/DLC1/RandomlyLunar/RandomlyLunar.asset").WaitForCompletion();    
+
+  public void Awake()
+  {
+    InitConfig();
+    LoadSprites();
+    DefineArtifact();
+
+    if (Chainloader.PluginInfos.ContainsKey("com.rune580.riskofoptions"))
+    {
+      ModSettingsManager.AddOption(new IntSliderOption(amount));
+      ModSettingsManager.AddOption(new ChoiceOption(removeFromPool));
+    }
+    
+    On.RoR2.Run.Start += (orig, self) =>
+    {
+      orig(self);
+      podRacingEnabled = RunArtifactManager.instance.IsArtifactEnabled(podRacing);
+      if (podRacingEnabled)
+      {
+        eulogy.tier = ItemTier.NoTier;
+        #pragma warning disable 0612, 0618
+        eulogy.deprecatedTier = ItemTier.NoTier;
+        #pragma warning restore 0612, 0618
+        PlayerCharacterMasterController._instances[0].master.inventory.GiveItem(eulogy.itemIndex, amount.Value);
+      }
+    };
+
+    On.RoR2.Run.RefreshLunarCombinedDropList += ( orig, self ) =>
+    {
+      orig(self);
+      podRacingEnabled = RunArtifactManager.instance.IsArtifactEnabled(podRacing);
+      if (podRacingEnabled)
+      {
+        if (removeFromPool.Value == BooleanChoice.True)
+        {
+          PickupIndex eulogyPickup = PickupCatalog.FindPickupIndex(eulogy.itemIndex);
+          PickupIndex beadsPickup = PickupCatalog.FindPickupIndex(beads.itemIndex);
+          self.availableLunarCombinedDropList.Remove(eulogyPickup);
+          self.availableLunarCombinedDropList.Remove(beadsPickup);
+        }
+      }
+    };
+
+    On.RoR2.Run.OnDestroy += (orig, self) =>
+    {
+      orig(self);
+      podRacingEnabled = false;
+    };
+  }
+
+  public void InitConfig()
+  {
+    amount = Config.Bind(
+      "General"
+    , "Amount"
+    , 2
+    , "The amount of Eulogy Zero to insert into the player's inventory on run start."
+    );
+    
+    removeFromPool = Config.Bind(
+      "General"
+    , "Remove Eulogy"
+    , BooleanChoice.True
+    , "Set to true to remove Eulogy Zero from the list of available lunars."
+    );
+  }
+
+  public void LoadSprites()
+  {
+    assets = AssetBundle.LoadFromFile(System.IO.Path.Combine(Paths.PluginPath, "RiskOfResources-ForceEulogy/artifactofpower"));
+    artifactOnIcon = assets.LoadAsset<Sprite>("artifactOn.png");
+    artifactOffIcon = assets.LoadAsset<Sprite>("artifactOff.png");
+  }
+
+  public void DefineArtifact()
+  {
+    podRacing.cachedName = "Artifact of Power";
+    podRacing.nameToken = "Artifact of Power";
+    podRacing.descriptionToken = "Converts a percentage of items in a run to lunar items.";
+    podRacing.smallIconSelectedSprite = artifactOnIcon;
+    podRacing.smallIconDeselectedSprite = artifactOffIcon;
+    podRacing.pickupModelPrefab = LegacyResourcesAPI.Load<ArtifactDef>("artifactdefs/MixEnemy").pickupModelPrefab;
+    ContentAddition.AddArtifactDef(podRacing);
+  }
+
+  public enum BooleanChoice
+  {
+    True,
+    False
+  }
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# ForceEulogy
+
+Companion mod for Gauntlet pod racing, adds an artifact that forces Eulogy Zero into the player's inventory at the start of the run then removes Eulogy Zero (and Beads of Fealty) from the item pool.
+
+# Changelog
+
+**1.1.0**
+
+-   Add Artifact of Power to allow easy enabling/disabling of ruleset
+-   Remove Beads of Fealty from the available item pool
+
+**1.0.2**
+
+-   Set default Eulogy count to 2
+
+**1.0.1**
+
+-   Make Eulogy NoTier to prevent stealing/cleansing/shrine of ordering
+
+**1.0.0**
+
+-   Remove Eulogy Zero from the list of available Lunar items in a run
+-   Insert {x} Eulogy Zero into the player's inventory at the start of the run
+
+**0.1.0**
+
+-   Project Setup
